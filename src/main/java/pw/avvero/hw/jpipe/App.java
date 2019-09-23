7162202feaa345f4c086a1dfb3d8b1fa249acb9e
belyaev-ay <fxdev-belyaev-ay@fxclub.org>
2019-09-23 10:40:35 +0700

    Using antlr in graal

--- src/main/java/pw/avvero/hw/jpipe/App.java
+++ src/main/java/pw/avvero/hw/jpipe/App.java
@@ -1,5 +1,13 @@
 package pw.avvero.hw.jpipe;
 
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import pw.avvero.hw.jpipe.antlr.HelloLexer;
+import pw.avvero.hw.jpipe.antlr.HelloParser;
+import pw.avvero.hw.jpipe.antlr.HelloWalker;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
@@ -12,6 +20,13 @@ public class App {
             while (true) {
                 if ((line = reader.readLine()) != null) {
                     System.out.println("echo>> " + line);
+
+                    HelloLexer lexer = new HelloLexer(CharStreams.fromString("hello world"));
+                    CommonTokenStream tokens = new CommonTokenStream(lexer);
+                    HelloParser parser = new HelloParser(tokens);
+                    ParseTree tree = parser.r();
+                    ParseTreeWalker walker = new ParseTreeWalker();
+                    walker.walk(new HelloWalker(), tree);
                 } else {
                     //input finishes
                     break;
---
+++ src/test/groovy/pw/avvero/hw/jpipe/AntlrHelloTests.groovy
@@ -0,0 +1,26 @@
+package pw.avvero.hw.jpipe
+
+import org.antlr.v4.runtime.CharStreams
+import org.antlr.v4.runtime.CommonTokenStream
+import org.antlr.v4.runtime.tree.ParseTree
+import org.antlr.v4.runtime.tree.ParseTreeWalker
+import pw.avvero.hw.jpipe.antlr.HelloBaseListener
+import pw.avvero.hw.jpipe.antlr.HelloLexer
+import pw.avvero.hw.jpipe.antlr.HelloParser
+import pw.avvero.hw.jpipe.antlr.HelloWalker
+import spock.lang.Specification
+
+class AntlrHelloTests extends Specification {
+
+    def "Hello test"() {
+        expect:
+        HelloLexer lexer = new HelloLexer(CharStreams.fromString("hello world"))
+        CommonTokenStream tokens = new CommonTokenStream(lexer)
+        HelloParser parser = new HelloParser(tokens);
+        ParseTree tree = parser.r();
+        ParseTreeWalker walker = new ParseTreeWalker();
+        walker.walk(new HelloWalker(), tree);
+
+    }
+
+}
